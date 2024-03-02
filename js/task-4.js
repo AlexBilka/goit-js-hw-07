c96cfba5468b3fa9b8baf91d3d1cb62eb7f89e2a
AlexBilka <aleksandrbelka@gmail.com>
2024-03-02 10:16:00 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -4,15 +4,9 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  // const elements = event.currentTarget.elements;
-  // console.log(event);
-  // console.log(elements);
-  // const emailNormalized = elements.email.value.trim();
-  // const passwordNormalized = elements.password.value.trim();
-
-  console.log(event);
-  const emailNormalized = event.currentTarget.elements.email.value.trim();
-  const passwordNormalized = event.currentTarget.elements.password.value.trim();
+  const elements = event.currentTarget.elements;
+  const emailNormalized = elements.email.value.trim();
+  const passwordNormalized = elements.password.value.trim();
 
   if (emailNormalized === '' || passwordNormalized === '') {
     alert('All form fields must be filled in');
@@ -24,15 +18,4 @@ function handleSubmit(event) {
     console.log(info);
     event.currentTarget.reset();
   }
-
-  // if (elements.email.value === '' || elements.password.value === '') {
-  //   alert('All form fields must be filled in');
-  // } else {
-  //   const info = {
-  //     email: emailNormalized,
-  //     password: passwordNormalized,
-  //   };
-  //   console.log(info);
-  //   event.currentTarget.reset();
-  // }
 }
